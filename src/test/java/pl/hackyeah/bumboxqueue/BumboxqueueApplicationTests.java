49f3a13eadb9852ace8637ca2efe6a13ce5a234d
maniekpl00 <maniek11pl@gmail.com>
2018-11-24 20:11:40 +0100

    Rename name of test

--- src/test/java/pl/hackyeah/bumboxqueue/BumboxqueueApplicationTests.java
+++ src/test/java/pl/hackyeah/bumboxqueue/BumboxqueueApplicationTests.java
@@ -7,11 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-<<<<<<< HEAD
-class zBumboxqueueApplicationTests {
-=======
 public class BumboxqueueApplicationTests {
->>>>>>> Implement HistoryEntity
 
 	@Test
 	public void contextLoads() {
