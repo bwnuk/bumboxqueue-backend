57e0ac87723ac74c9606a881f4d708b21247718e
Bartłomiej Kluska <piaskunbk@gmail.com>
2018-11-24 14:13:28 +0100

    Implement doctor entity

---
+++ src/main/java/pl/hackyeah/bumboxqueue/service/DoctorService.java
@@ -0,0 +1,16 @@
+package pl.hackyeah.bumboxqueue.service;
+
+import pl.hackyeah.bumboxqueue.dto.doctorDto;
+
+import java.util.List;
+
+public class DoctorService {
+
+
+
+    public List<doctorDto> findAll()
+    {
+        return null;
+
+    }
+}
