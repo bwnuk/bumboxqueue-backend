4de7127adc02eb6e0177248f282c42a4bf3a2747
maniekpl00 <maniek11pl@gmail.com>
2018-11-24 12:04:49 +0100

    Init project

---
+++ src/main/java/pl/hackyeah/bumboxqueue/BumboxqueueApplication.java
@@ -0,0 +1,12 @@
+package pl.hackyeah.bumboxqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumboxqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumboxqueueApplication.class, args);
+	}
+}
