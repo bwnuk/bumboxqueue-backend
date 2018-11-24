e48343165f7b3a00d90cb4a0b2b2cad6bd8b58ab
Bartłomiej Kluska <piaskunbk@gmail.com>
2018-11-24 15:54:31 +0100

    changed this shit

--- src/main/java/pl/hackyeah/bumboxqueue/dto/DoctorDto.java
+++ src/main/java/pl/hackyeah/bumboxqueue/dto/DoctorDto.java
@@ -2,7 +2,6 @@ package pl.hackyeah.bumboxqueue.dto;
 
 
 import lombok.Data;
-import pl.hackyeah.bumboxqueue.error.BadRequestException;
 
 import javax.validation.constraints.Null;
 
@@ -14,6 +13,30 @@ public class DoctorDto {
 
 
     public void validate() throws Exception {
+        if(name.length() == 0)
+        {
+            throw new NullPointerException("No name parameter");
+        }
+        if(name.length() > 30)
+        {
+            throw new Exception("Name too long");
+        }
+        if(surname.length() == 0)
+        {
+            throw new NullPointerException("No surname parameter");
+        }
+        if(surname.length() > 30)
+        {
+            throw new Exception("Surname too long");
+        }
+        if(specialization.length() == 0)
+        {
+            throw new NullPointerException("No specialization parameter");
+        }
+        if(specialization.length() > 30)
+        {
+            throw new Exception("specialization too long");
+        }
 
     }
 }
